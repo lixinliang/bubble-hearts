5c5c387f49c178cba11e352b2e113279dddd615c
李昕亮 <lixinliang@yy.com>
2017-01-12 09:34:23 +0800

    add rollup
    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import path from 'path';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import license from 'rollup-plugin-license';
+
+export default {
+    format : 'umd',
+    entry : 'bubble-hearts.js',
+    dest : 'bubble-hearts.min.js',
+    moduleName : 'BubbleHearts',
+    plugins : [
+        babel({
+            presets : ['es2015-rollup'],
+        }),
+        uglify(),
+        license({
+            banner : {
+                file : path.join(__dirname, 'banner'),
+            },
+        }),
+    ],
+};
