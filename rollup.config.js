98efd1905f783aa576292a1308022d29974479c8
lixinliang <393464140@qq.com>
2018-09-27 09:37:55 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
-'use strict';
+'use strict'
 
-import path from 'path';
-import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
-import license from 'rollup-plugin-license';
+import path from 'path'
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import license from 'rollup-plugin-license'
 
 export default {
-    format : 'umd',
-    entry : 'bubble-hearts.js',
-    dest : 'bubble-hearts.min.js',
-    moduleName : 'BubbleHearts',
-    plugins : [
-        babel({
-            presets : ['es2015-rollup'],
-        }),
-        uglify(),
-        license({
-            banner : {
-                file : path.join(__dirname, 'banner'),
-            },
-        }),
-    ],
-};
+  format: 'umd',
+  entry: 'bubble-hearts.js',
+  dest: 'bubble-hearts.min.js',
+  moduleName: 'BubbleHearts',
+  plugins: [
+    babel({
+      presets: ['es2015-rollup']
+    }),
+    uglify(),
+    license({
+      banner: {
+        file: path.join(__dirname, 'banner')
+      }
+    })
+  ]
+}
